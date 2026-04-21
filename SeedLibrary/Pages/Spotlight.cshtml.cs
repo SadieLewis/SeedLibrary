860e9a2f8c8efe0b904a5044b7e5ecfbc613e9fa
Clodagh <nanglecc@hendrix.edu>
2026-04-21 21:48:40 +0100

    Seed Spotlight Stand In Page

---
+++ SeedLibrary/Pages/Spotlight.cshtml.cs
@@ -0,0 +1,21 @@
+using SeedLibrary.Models.SchoolViewModels;
+using SeedLibrary.Data;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SeedLibrary.Models;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SeedLibrary.Pages;
+
+public class SpotlightSeeds : PageModel
+{
+    public void OnGet()
+    {
+
+    }
+}
