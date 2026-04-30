e67364aa814e233292065c98a3d106de0707526b
LoganO03 <j.logan.oroark@gmail.com>
2026-04-30 16:37:53 -0500

    Potential updated SeedContext

--- SeedLibrary/Data/SeedContext.cs
+++ SeedLibrary/Data/SeedContext.cs
@@ -14,15 +14,60 @@ namespace SeedLibrary.Data
         {
         }
 
-        public DbSet<Seed> Seeds{get;set;}
-        public DbSet<Enrollment> Enrollments { get; set; }
-        public DbSet<Course> Courses { get; set; }
-
+        public DbSet<SeedPacket> SeedPackets => Set<SeedPacket>();
+        public DbSet<Variety> Varieties => Set<Variety>();
+        public DbSet<CommonName> CommonNames => Set<CommonName>();
+        public DbSet<Source> Sources => Set<Source>();
+        public DbSet<Date> Dates => Set<Date>();
+        public DbSet<Donation> Donations => Set<Donation>();
+        public DbSet<Growing> Growings => Set<Growing>();
+    
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Course>().ToTable("Course");
-            modelBuilder.Entity<Enrollment>().ToTable("Enrollment");
-            modelBuilder.Entity<Seed>().ToTable("Seed");
+            modelBuilder.Entity<SeedPacket>().ToTable("SeedPacket")
+                .HasKey(s => s.SeedId);
+            modelBuilder.Entity<Variety>().ToTable("Variety")
+                .HasKey(v => v.VarietyName);
+            modelBuilder.Entity<CommonName>().ToTable("CommonName")
+                .HasKey(c => c.Name);
+            modelBuilder.Entity<Source>().ToTable("Source")
+                .HasKey(s => s.Id);
+            modelBuilder.Entity<Date>().ToTable("Date")
+                .HasKey(d => d.Id);
+            modelBuilder.Entity<Donation>().ToTable("Donation")
+                .HasKey(d => new { d.SourceId, d.SeedId, d.Year });
+            modelBuilder.Entity<Growing>().ToTable("Growing")
+                .HasKey(g => new { g.DatesId, g.SeedId });
+            modelBuilder.Entity<SeedPacket>()
+                .HasOne(s => s.Variety)
+                .WithMany(v => v.SeedPackets)
+                .HasForeignKey(s => s.VarietyName);
+            modelBuilder.Entity<Variety>()
+                .HasOne(v => v.CommonName)
+                .WithMany(c => c.Varieties)
+                .HasForeignKey(v => v.CommonNameName);
+            
+            modelBuilder.Entity<Donation>()
+                .HasOne(d => d.Source)
+                .WithMany(s => s.Donations)
+                .HasForeignKey(d => d.SourceId);
+
+            modelBuilder.Entity<Donation>()
+                .HasOne(d => d.SeedPacket)
+                .WithMany(s => s.Donations)
+                .HasForeignKey(d => d.SeedId);
+
+            modelBuilder.Entity<Growing>()
+                .HasOne(g => g.Date)
+                .WithMany(d => d.Growings)
+                .HasForeignKey(g => g.DatesId);
+
+            modelBuilder.Entity<Growing>()
+                .HasOne(g => g.SeedPacket)
+                .WithMany(s => s.Growings)
+                .HasForeignKey(g => g.SeedId);
+            
+            
         }
     }
 }
